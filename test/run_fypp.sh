ab135245095b32f602e239b49fddc8a84779ee46
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-05-17 09:19:05 +0900

    Revert "Add tests for the intrinsic functions"
    This reverts commit db4dc7b74611f14daed5e033fdf0b5a11f63d4f0.

--- test/run_fypp.sh
+++ test/run_fypp.sh
@@ -1,7 +1,3 @@
 #!/bin/sh -e
-fypp -F -DMODE=1 test_ieee_is_X.fypp test_ieee_is_finite.f90
-fypp -F -DMODE=2 test_ieee_is_X.fypp test_ieee_is_nan.f90
-fypp -F -DMODE=3 test_ieee_is_X.fypp test_ieee_is_negative.f90
-
 fypp -F test_is_ieee_class.fypp  test_is_ieee_class.f90
 fypp -F test_set_ieee_class.fypp test_set_ieee_class.f90
