81b78872028ff9b21409fd8886b28163c2a4b9bd
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-05-25 08:29:52 +0900

    Update test `program` for `test_is_ieee_*` → `submodule` for `test_is_ieee_*`

--- run_fpm_test.sh
+++ run_fpm_test.sh
@@ -6,9 +6,15 @@ fypp -F          test_preconditions.fypp      test_preconditions.f90
 fypp -F -DMODE=1 test_ieee_is_X.fypp          test_ieee_is_finite.f90
 fypp -F -DMODE=2 test_ieee_is_X.fypp          test_ieee_is_nan.f90
 fypp -F -DMODE=3 test_ieee_is_X.fypp          test_ieee_is_negative.f90
+fypp -F -DMODE=1 test_is_ieee_X.fypp          test_is_ieee_negative_inf.f90
+fypp -F -DMODE=2 test_is_ieee_X.fypp          test_is_ieee_negative_zero.f90
+fypp -F -DMODE=3 test_is_ieee_X.fypp          test_is_ieee_either_zero.f90
+fypp -F -DMODE=4 test_is_ieee_X.fypp          test_is_ieee_positive_zero.f90
+fypp -F -DMODE=5 test_is_ieee_X.fypp          test_is_ieee_positive_inf.f90
+fypp -F -DMODE=6 test_is_ieee_X.fypp          test_is_ieee_quiet_nan.f90
+fypp -F -DMODE=7 test_is_ieee_X.fypp          test_is_ieee_signaling_nan.f90
 fypp -F          test.fypp                    test.f90
 
-fypp -F test_is_ieee_class.fypp  test_is_ieee_class.f90
 fypp -F test_set_ieee_class.fypp test_set_ieee_class.f90
 
 cd ..
