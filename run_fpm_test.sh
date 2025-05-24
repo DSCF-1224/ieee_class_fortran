b329facd30bf17961c28150be6122d97a77f9f6f
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-05-25 07:46:45 +0900

    Update test `program` for `test_ieee_is_*` → `submodule` for `test_ieee_is_*`

--- run_fpm_test.sh
+++ run_fpm_test.sh
@@ -1,13 +1,12 @@
 #!/bin/sh -e
 cd test
 
-fypp -F ieee_class_fortran_test.fypp ieee_class_fortran_test.f90
-fypp -F test_preconditions.fypp      test_preconditions.f90
-fypp -F test.fypp                    test.f90
-
-fypp -F -DMODE=1 test_ieee_is_X.fypp test_ieee_is_finite.f90
-fypp -F -DMODE=2 test_ieee_is_X.fypp test_ieee_is_nan.f90
-fypp -F -DMODE=3 test_ieee_is_X.fypp test_ieee_is_negative.f90
+fypp -F          ieee_class_fortran_test.fypp ieee_class_fortran_test.f90
+fypp -F          test_preconditions.fypp      test_preconditions.f90
+fypp -F -DMODE=1 test_ieee_is_X.fypp          test_ieee_is_finite.f90
+fypp -F -DMODE=2 test_ieee_is_X.fypp          test_ieee_is_nan.f90
+fypp -F -DMODE=3 test_ieee_is_X.fypp          test_ieee_is_negative.f90
+fypp -F          test.fypp                    test.f90
 
 fypp -F test_is_ieee_class.fypp  test_is_ieee_class.f90
 fypp -F test_set_ieee_class.fypp test_set_ieee_class.f90
