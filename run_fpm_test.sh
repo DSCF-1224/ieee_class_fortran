947e056e0375a1b861ae5ede0fb86ca5f12715b6
DSCF_1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-05-25 20:59:24 +0900

    Develop & Fix test (#21)
    * Update test
    .f90" files for testing are generated when needed.
    
    * Update test
    Update workflow for the test
    
    * Update test
    Add a `program` to test `ieee_class_fortran`
    
    * Update test
    Add a `subroutine` to test the preconditions
    
    * Update test
    `program` for `test_ieee_is_*` → `submodule` for `test_ieee_is_*`
    
    * Update test
    `program` for `test_is_ieee_*` → `submodule` for `test_is_ieee_*`
    
    * Adjust files for fypp
    The associated .f90 files has been modified,
    but the behavior of the implemented functions/subroutines remain unchanged.
    
    * Update test
    * `program` for `test_set_ieee_*` → `submodule` for `test_set_ieee_*`
    * Fix the test contents

--- run_fpm_test.sh
+++ run_fpm_test.sh
@@ -1,2 +1,26 @@
 #!/bin/sh -e
+cd test
+
+fypp -F          ieee_class_fortran_test.fypp ieee_class_fortran_test.f90
+fypp -F          test_preconditions.fypp      test_preconditions.f90
+fypp -F -DMODE=1 test_ieee_is_X.fypp          test_ieee_is_finite.f90
+fypp -F -DMODE=2 test_ieee_is_X.fypp          test_ieee_is_nan.f90
+fypp -F -DMODE=3 test_ieee_is_X.fypp          test_ieee_is_negative.f90
+fypp -F -DMODE=1 test_is_ieee_X.fypp          test_is_ieee_negative_inf.f90
+fypp -F -DMODE=2 test_is_ieee_X.fypp          test_is_ieee_negative_zero.f90
+fypp -F -DMODE=3 test_is_ieee_X.fypp          test_is_ieee_either_zero.f90
+fypp -F -DMODE=4 test_is_ieee_X.fypp          test_is_ieee_positive_zero.f90
+fypp -F -DMODE=5 test_is_ieee_X.fypp          test_is_ieee_positive_inf.f90
+fypp -F -DMODE=6 test_is_ieee_X.fypp          test_is_ieee_quiet_nan.f90
+fypp -F -DMODE=7 test_is_ieee_X.fypp          test_is_ieee_signaling_nan.f90
+fypp -F -DMODE=1 test_set_ieee_X.fypp         test_set_ieee_negative_inf.f90
+fypp -F -DMODE=2 test_set_ieee_X.fypp         test_set_ieee_negative_zero.f90
+fypp -F -DMODE=3 test_set_ieee_X.fypp         test_set_ieee_positive_zero.f90
+fypp -F -DMODE=4 test_set_ieee_X.fypp         test_set_ieee_positive_inf.f90
+fypp -F -DMODE=5 test_set_ieee_X.fypp         test_set_ieee_quiet_nan.f90
+fypp -F -DMODE=6 test_set_ieee_X.fypp         test_set_ieee_signaling_nan.f90
+fypp -F          test.fypp                    test.f90
+
+cd ..
+
 fpm test --flag "-Wall -Wextra -Werror -pedantic -std=f2008 -ffree-line-length-none"
